f47b29100802c81b5635d438b830a80364ee0f7c
bernat <andikasukardi1@gmail.com>
2025-07-23 03:46:48 -0700

    modif file index hal toko dan tambah file proc.php

--- pages/toko/index.php
+++ pages/toko/index.php
@@ -1,41 +1,93 @@
-<?php
-$label = 'Profil';
-include_once "../../header.php";
-if (!valid()) {
-    echo "<script>window.location='" . base_url() . "'</script>";
-    exit();
-}
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $user = trim(mysqli_real_escape_string($db, $_POST['username']));
-    $pass = trim(mysqli_real_escape_string($db, $_POST['password']));
-    $encpass = sha1($pass);
-    $id = $_POST['id'];
-    mysqli_query($db, "UPDATE tb_user SET username = '$user', password = '$encpass' WHERE id_user = '$id'") or die(mysqli_error($db));
-    $sql = mysqli_query($db, "SELECT * FROM tb_user WHERE id_user = '$id'") or die(mysqli_error($db));
-    $_SESSION['data'] = mysqli_fetch_assoc($sql);
-    $_SESSION['pass'] = $pass;
-    echo "<script>window.location='" . base_url('pages/profil/') . "'</script>";
-    exit();
-}
-?>
+<!-- ini dpe kode untuk menampilkan data lokasi -->
 
-<h4><?= $label ?></h4>
-<form class="profil-form" method="post">
-    <input type="hidden" name="id" value="<?= $_SESSION['data']['id_user'] ?>">
-    <div class="d-flex gap-4 groupnya">
-        <div class="inputan">
-            <div class="input-group-addon"><i class="fas fa-user"></i></div>
-            <input value="<?= $_SESSION['data']['username'] ?>" class="form-control" type="text" name="username" id=""
-                required>
-        </div>
-        <div class="inputan">
-            <div class="input-group-addon"><i class="fas fa-lock"></i></div>
-            <input value="<?= $_SESSION['pass'] ?>" class="form-control" type="password" name="password" id="" required>
-        </div>
+<?php
+$label = 'Toko Anda';
+include_once "../../header.php";
+if (!has_akses(['pemilik-toko'])) {
+    echo "<script>window.location='" . base_url() . "'</script>";
+}
+$ada = false;
+$id = $_SESSION['data']['id_lokasi'];
+if ($id == 0) {
+    $ada = true;
+}
+
+$sql = mysqli_query($db, "SELECT * FROM tb_lokasi WHERE id_lokasi = '$id'") or die(mysqli_error($db));
+$data = mysqli_fetch_assoc($sql); ?>
+
+<div class="d-flex justify-content-between">
+    <div>
+        <h4><?= $label ?></h4>
     </div>
-    <div class="mt-3">
-        <input onclick="return confirm('Anda yakin untuk diubah profilnya?')" type="submit" class="py-2 px-4 btn btn-info btn-sm" value="Ubah">
+</div>
+
+
+<form action="proc.php" method="post" class="d-flex gap-3" enctype="multipart/form-data">
+    <div style="font-size: 1rem; width: 100%; border-radius: 16px" class="p-3 border">
+        <div class="row mb-2">
+            <div class="col-4">Nama Toko</div>
+            <input class="col-7 form-control" type="text" name="nama_toko" value=<?= $data['nama_toko'] ?? '-' ?>>
+        </div>
+        <div class="row mb-2">
+            <div class="col-4">Alamat</div>
+            <input class="col-7 form-control" type="text" name="alamat" value=<?= $data['alamat'] ?? '-' ?>>
+        </div>
+        <div class="row mb-2">
+            <div class="col-4">Kontak</div>
+            <input class="col-7 form-control" type="text" name="kontak" value=<?= $data['kontak'] ?? '-' ?>>
+        </div>
+        <div class="row mb-2">
+            <div class="col-4">Hari</div>
+            <input class="col-7 form-control" type="text" name="hari" value=<?= $data['hari'] ?? '-' ?>>
+        </div>
+        <div class="row mb-2">
+            <div class="col-4">Waktu</div>
+            <input class="col-7 form-control" type="text" name="waktu" value=<?= $data['waktu'] ?? '-' ?>>
+        </div>
+        <div class="row mb-2">
+            <div class="col-4">Latitude</div>
+            <input class="col-7 form-control" type="text" name="latti" value=<?= $data['latti'] ?? '-' ?>>
+        </div>
+        <div class="row mb-2">
+            <div class="col-4">Longitude</div>
+            <input class="col-7 form-control" type="text" name="longi" value=<?= $data['longi'] ?? '-' ?>>
+        </div>
+
+
+        <?php
+        if ($ada) { ?>
+            <div class="d-flex border-bottom mb-3">
+                <div class="d-flex flex-column">
+                    <span style="font-size: 1.50rem;">Gambar</span>
+                    <input id="gambar" type="file" accept="image/*" name="gambar" required>
+                </div>
+            </div>
+            <input class="mt-3 btn btn-sm btn-info py-2 px-4" type="submit" name="add" value="Simpan">
+        <?php } else { ?>
+            <div class="d-flex border-bottom mb-3">
+                <div class="d-flex flex-column">
+                    <span style="font-size: 1.50rem;">Gambar</span>
+                    <input id="gambar" type="file" name="gambar">
+                    <p class="small">Kosongkan jika tidak ingin mengubah gambar</p>
+                </div>
+                <?php if (!empty($data['gambar'])): ?>
+                    <p><img src="../../_assets/images/<?= $data['gambar'] ?>" alt="Gambar TPQ"
+                            style="max-width: 120px; height: auto;"></p>
+                <?php endif; ?>
+            </div>
+            <input class="mt-3 btn btn-sm btn-info py-2 px-4" type="submit" name="edit" value="Ubah">
+        <?php } ?>
+
     </div>
+
+    <?php if (!empty($data['gambar'])): ?>
+        <img src="../../_assets/images/<?= $data['gambar'] ?>" alt="Gambar TPQ" class="border"
+            style="padding: 10px; max-width: 300px; height: 300px; border-radius: 16px;">
+    <?php else: ?>
+        <p>Tidak ada gambar yang tersedia.</p>
+    <?php endif; ?>
+
+
 </form>
 
 <?php include_once "../../footer.php" ?>
---
+++ pages/toko/proc.php
@@ -0,0 +1,85 @@
+<!-- sedangkan kode ini untuk tangani logika di belakang layar -->
+ <!-- yang tangan query tambah dan hapus data lokasi -->
+
+<?php
+require_once '../../config.php';
+if (!valid()) {
+    echo "<script>window.location='" . base_url() . "'</script>";
+    exit();
+}
+function trimtext($db, $data)
+{
+    return trim(mysqli_escape_string($db, $data));
+}
+
+function uploadgambar($img, $targetdir, $extimg = null)
+{
+    $allowtypes = ['jpg', 'png', 'jpeg', 'gif'];
+    if (!empty($img['name'])) {
+        $filename = basename($img['name']);
+        $targetfilepath = $targetdir . $filename;
+        $filetype = strtolower(pathinfo($targetfilepath, PATHINFO_EXTENSION));
+        if (in_array($filetype, $allowtypes)) {
+            if (move_uploaded_file($img["tmp_name"], $targetfilepath)) {
+                return $filename;
+            } else {
+                echo "Gagal mengunggah gambar!";
+                exit();
+            }
+        } else {
+            echo "Format file tidak didukung. Gunakan JPG, PNG, JPEG, atau GIF.";
+            exit();
+        }
+    }
+
+    return $extimg;
+}
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    $targetdir = "../../_assets/images/";
+
+    if (isset($_POST["add"])) {
+        $nama = trimtext($db, $_POST["nama_toko"]);
+        $alamat = trimtext($db, $_POST["alamat"]);
+        $kontak = trimtext($db, $_POST["kontak"]);
+        $hari = trimtext($db, $_POST["hari"]);
+        $waktu = trimtext($db, $_POST["waktu"]);
+        $latti = trimtext($db, $_POST["latti"]);
+        $longi = trimtext($db, $_POST["longi"]);
+
+        $gambar = uploadgambar($_FILES["gambar"], $targetdir);
+
+        mysqli_query($db, "INSERT INTO tb_lokasi (nama_toko, alamat, kontak, hari, waktu, gambar, latitude, longitude) 
+                    VALUES ('$nama', '$alamat', $kontak, '$hari', '$waktu', '$gambar', '$latti', '$longi')") or die(mysqli_error($db));
+
+        echo "<script>window.location='" . base_url('pages/toko/') . "'</script>";
+        exit();
+    } elseif (isset($_POST['edit'])) {
+        $id = $_SESSION['data']['id_lokasi'];
+        $nama = trimtext($db, $_POST["nama_toko"]);
+        $alamat = trimtext($db, $_POST["alamat"]);
+        $kontak = trimtext($db, $_POST["kontak"]);
+        $hari = trimtext($db, $_POST["hari"]);
+        $waktu = trimtext($db, $_POST["waktu"]);
+        $latti = trimtext($db, $_POST["latti"]);
+        $longi = trimtext($db, $_POST["longi"]);
+
+        $extsql = mysqli_query($db, "SELECT * FROM tb_lokasi WHERE id_lokasi = '$id'") or die(mysqli_error($db));
+        $extimgdata = mysqli_fetch_assoc($extsql);
+
+        $gambar = uploadgambar($_FILES['gambar'], $targetdir, $extimgdata['gambar'] ?? null);
+        $gambarquery = "";
+        if (!empty($extimgdata)) {
+            $gambarquery .= "gambar = '{$gambar}', ";
+        }
+
+        mysqli_query($db, "UPDATE tb_lokasi SET nama_toko = '$nama', alamat = '$alamat', kontak = '$kontak', hari = '$hari', waktu = '$waktu', $gambarquery latitude = '$latti', longitude = '$longi' WHERE id_lokasi = '$id'") or die(mysqli_error($db));
+
+        echo "<script>window.location='" . base_url('pages/toko/') . "'</script>";
+        exit();
+    } else {
+        exit();
+    }
+} else {
+    exit();
+}
